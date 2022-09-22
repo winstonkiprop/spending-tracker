64515a4a28f34657f7ac19f1162ce0ddc5c14cf5
winston kiprop <winstonekip50@gmail.com>
2022-09-22 12:39:01 +0300

    create components folder

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+function Home(){
+    return(
+        <div>
+            <header>
+            <h1 app-name>Spending <span>Tracker</span></h1>
+            </header>
+            <div className="head-sect">
+                <article>
+                    <p>Track your expenses.</p>
+                    <p>Access the various categories of items and add their related costs.</p>
+                </article>
+            </div>
+        </div>
+    )
+}
